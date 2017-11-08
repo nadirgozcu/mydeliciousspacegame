976d0c186cd6c624e83f7eebdac0bbb7356c866e
nadirgozcu <nadirgozcu@hotmail.com>
2017-11-08 23:11:53 +0100

    IMPROVEMENT: Add function to reposition stars over time

--- System/StarManager.cs
+++ System/StarManager.cs
@@ -20,15 +20,36 @@ public class StarManager : MonoBehaviour
         Vector2 newPos = tar + dir;
         other.transform.position = new Vector3(newPos.x, newPos.y, str.z);
     }
-    
-    void Start()
+
+    IEnumerator InstantiateStarsOverTime()
     {
+        for (int i = 0; i < starRoot.transform.GetChildCount(); i++)
+        {
+            if (!PlayerCharacter.instance)
+                break;
+            Vector2 randomPoint = Mathf2.SelectRandomPoint(radius);
+            Vector3 randomPos = new Vector3(randomPoint.x + PlayerCharacter.instance.tr.position.x, randomPoint.y + PlayerCharacter.instance.tr.position.y, UnityEngine.Random.Range(10, 30));
+            starRoot.transform.GetChild(i).transform.position = randomPos;
+            if(i % 20 == 0)
+                yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void RePositionStars()
+    {
+        StartCoroutine(InstantiateStarsOverTime());
+    }
+
+    public void Init()
+    {
+        instance = this;
         tr = transform;
         radius = GetComponent<CircleCollider2D>().bounds.size.x/2;
         for (int i = 0; i < starCount; i++)
         {
             Vector2 randomPoint = Mathf2.SelectRandomPoint(radius);
-            GameObject star = GameObject.Instantiate(starObj, new Vector3(randomPoint.x + tr.position.x, randomPoint.y + tr.position.y, UnityEngine.Random.Range(10, 30)), Quaternion.identity);
+            Vector3 randomPos = new Vector3(randomPoint.x + PlayerCharacter.instance.tr.position.x, randomPoint.y + PlayerCharacter.instance.tr.position.y, UnityEngine.Random.Range(10, 30));
+            GameObject star = GameObject.Instantiate(starObj, randomPos, Quaternion.identity);
             star.transform.SetParent(starRoot.transform);
             star.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
         }
